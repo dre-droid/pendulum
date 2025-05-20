38a33f88d51d1b06e296a7f535f726da7c95ded1
AndreaMastroberti <aamastroberti@gmail.com>
2025-05-20 16:52:09 +0200

    added enable pin control to motor driver

--- motor_test.py
+++ motor_test.py
@@ -7,12 +7,14 @@ GPIO.setmode(GPIO.BCM)
 # Motor pins
 IN1_PIN = 12  # GPIO12 for IN1
 IN2_PIN = 13  # GPIO13 for IN2
+EN_PIN = 6   # GPIO6 for EN (Enable)
 PWM_FREQ = 1000  # 1kHz PWM frequency
 DUTY_CYCLE = 10  # 10% duty cycle for low power test
 
-# Setup PWM pins
+# Setup pins
 GPIO.setup(IN1_PIN, GPIO.OUT)
 GPIO.setup(IN2_PIN, GPIO.OUT)
+GPIO.setup(EN_PIN, GPIO.OUT)
 pwm_in1 = GPIO.PWM(IN1_PIN, PWM_FREQ)
 pwm_in2 = GPIO.PWM(IN2_PIN, PWM_FREQ)
 
@@ -20,6 +22,10 @@ def test_motor():
     try:
         print("Starting motor test...")
         print(f"Running at {DUTY_CYCLE}% power")
+        
+        # Enable the motor driver by setting EN pin high
+        GPIO.output(EN_PIN, GPIO.HIGH)
+        print("Motor driver enabled")
         
         # Start PWM on both pins
         pwm_in1.start(DUTY_CYCLE)
@@ -39,6 +45,7 @@ def test_motor():
         # Cleanup
         pwm_in1.stop()
         pwm_in2.stop()
+        GPIO.output(EN_PIN, GPIO.LOW)  # Disable motor driver
         GPIO.cleanup()
         print("GPIO cleanup complete")
 
